dc043e606b0ad0309c42b972efa1424bac7afaf4
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-25 15:48:01 -0700

    Initial Commit

---
+++ DisplayingMap-SwiftUI/DisplayingMap_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayingMap_SwiftUIApp.swift
+//  DisplayingMap-SwiftUI
+//
+//  Created by Mitya Kim on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct DisplayingMap_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
